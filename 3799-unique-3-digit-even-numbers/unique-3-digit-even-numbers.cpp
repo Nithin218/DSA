d309c99998030a1cad95d12687459e2d02e0d700
Nithin_Nagireddy <122455484+Nithin218@users.noreply.github.com>
2026-09-13 02:28:04 +0530

    Time: 2 ms (69.23%) | Memory: 28.7 MB (82.02%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int totalNumbers(vector<int>& digits) {
+
+        vector<int> freq(10, 0);
+
+        for (int x : digits) {
+            freq[x]++;
+        }
+
+        int ans = 0;
+
+        // Last digit must be even
+        for (int last = 0; last <= 8; last += 2) {
+
+            if (freq[last] == 0)
+                continue;
+
+            freq[last]--;
+
+            // First digit cannot be zero
+            for (int first = 1; first <= 9; first++) {
+
+                if (freq[first] == 0)
+                    continue;
+
+                freq[first]--;
+
+                // Count DISTINCT possible middle digits
+                for (int middle = 0; middle <= 9; middle++) {
+
+                    if (freq[middle] > 0) {
+                        ans++;
+                    }
+                }
+
+                freq[first]++;
+            }
+
+            freq[last]++;
+        }
+
+        return ans;
+    }
+};
